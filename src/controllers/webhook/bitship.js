29c8852977bec0b403cf1995d06644b3a2876fd1
Misdar <misdarmanto1922@gmail.com>
2025-12-22 21:36:51 +0700

    fix biteship webhook

--- src/controllers/webhook/bitship.js
+++ src/controllers/webhook/bitship.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bitshipWebhookHandler = void 0;
 const orders_1 = require("../../models/orders");
@@ -9,27 +11,33 @@ const response_1 = require("../../utilities/response");
 const http_status_codes_1 = require("http-status-codes");
 const logs_1 = __importDefault(require("../../logs"));
 const bitshipWebhookHandler = async (req, res) => {
-    const payload = req.body;
-    try {
-        const order = await orders_1.OrdersModel.findOne({
-            where: {
-                deleted: 0,
-                orderWaybillId: payload.courier_waybill_id
-            }
-        });
-        if (order) {
-            if (payload.status === 'delivered') {
-                order.orderStatus = 'done';
-                order.save();
-            }
+  const payload = req.body;
+  try {
+    console.log("====pas 1");
+    if (payload && payload.courier_waybill_id) {
+      const order = await orders_1.OrdersModel.findOne({
+        where: {
+          deleted: 0,
+          orderWaybillId: payload?.courier_waybill_id,
+        },
+      });
+      if (order) {
+        if (payload.status === "delivered") {
+          order.orderStatus = "done";
+          order.save();
         }
-        return res.status(http_status_codes_1.StatusCodes.OK).json(response_1.ResponseData.default);
+      }
+    } else {
+      logs_1.default.warn("[BITESHIP WEBHOOK] - payload is empty");
     }
-    catch (error) {
-        logs_1.default.error('Bitship Webhook Error:', error);
-        return res
-            .status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR)
-            .json(response_1.ResponseData.error('Webhook processing failed'));
-    }
+    return res
+      .status(http_status_codes_1.StatusCodes.OK)
+      .json(response_1.ResponseData.default);
+  } catch (error) {
+    logs_1.default.error("Bitship Webhook Error:", error);
+    return res
+      .status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(response_1.ResponseData.error("Webhook processing failed"));
+  }
 };
 exports.bitshipWebhookHandler = bitshipWebhookHandler;
