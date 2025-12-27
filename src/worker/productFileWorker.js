eb3e98849b5da43f8f41068588729596ff132020
Misdar <misdarmanto1922@gmail.com>
2025-12-27 22:39:35 +0700

    feat: uopdate

--- src/worker/productFileWorker.js
+++ src/worker/productFileWorker.js
@@ -12,9 +12,7 @@ const products_1 = require("../models/products");
 const fileUpload_1 = require("../models/fileUpload");
 const configs_1 = require("../configs");
 const logs_1 = __importDefault(require("../logs"));
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const priceCalculator_1 = require("../utilities/priceCalculator");
 new bullmq_1.Worker(
   "product-file-queue",
   async (job) => {
@@ -27,27 +25,23 @@ new bullmq_1.Worker(
       { where: { fileId: fileId } }
     );
     try {
-      // Simulasi proses file besar (misalnya 10 detik)
-      console.log(`[WORKER] Simulating heavy processing...`);
-      await sleep(10000 * 2); // 10 detik
       const workbook = xlsx_1.default.readFile(filePath);
       const sheet = workbook.SheetNames[0];
       const data = xlsx_1.default.utils.sheet_to_json(workbook.Sheets[sheet]);
-      // Simulasi proses baris demi baris (misalnya setiap row 1 detik)
-      for (let i = 0; i < data.length; i++) {
-        console.log(`[WORKER] Processing row ${i + 1}/${data.length}`);
-        await sleep(1000);
-      }
       const products = data.map((item) => ({
         productName: item.nama,
         productDescription: item.deskripsi,
         productImages: [item.image],
-        productPrice: Number(item.harga) || 0,
+        productPrice: item.harga || 0,
         productCategoryId: item.kategori,
         productSubCategoryId: item.subkategori,
         productCode: item.kode || "",
         productStock: Number(item.stok) || 0,
         productDiscount: Number(item.diskon) || 0,
+        productSellPrice: (0, priceCalculator_1.calculateSellPrice)({
+          originalPrice: Number(item.harga),
+          discountPercent: Number(item.diskon),
+        }),
         productWeight: Number(item.berat) || 0,
       }));
       await products_1.ProductModel.bulkCreate(products);
